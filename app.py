6de233bd275822b2ddd1c8db6709d5573344adbb
StefTheChef17 <stefan.stevanovic.rs@gmail.com>
2026-03-24 17:37:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 import os
+import sqlite3
+from flask import flask, reguest, jsonify, json
+from flask-cors import CORS
+
+app = Flask(_name_)
+
+@app.route("/")
+def index():
+  return "Zdravo programeri"
+
+
+if _name_=='__main__':
+  app.run()
